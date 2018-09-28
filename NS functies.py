d8b32efe6bcc0d8a7c4e33d1928228759a35e895
pim <pim.vermeij@student.hu.nl>
2018-09-28 10:15:43 +0100

    first commit

---
+++ NS functies.py
@@ -0,0 +1,26 @@
+def standaardPrijs(afstandKM):
+    if afstandKM < 50:
+        prijs = afstandKM * 0.8
+    elif afstandKM <= 0:
+        prijs = 0
+    elif afstandKM >= 50:
+        prijs = 15 + (afstandKM * 0.6)
+    return prijs
+def ritprijs(leeftijd, weekendrit, afstandKM):
+    if leeftijd < 12 or leeftijd >= 65 and weekendrit == True:
+        standaardPrijs(afstandKM) * 0.65
+    elif leeftijd < 12 or leeftijd >= 65:
+        standaardPrijs(afstandKM) * 0.7
+    elif leeftijd >= 12 and leeftijd < 65 and weekendrit == True:
+        standaardPrijs(afstandKM) * 0.6
+    else:
+        standaardPrijs(afstandKM)
+    print(standaardPrijs(afstandKM))
+leeftijd = float(input("hoe oud ben je?"))
+weekendrit = input ("is het weekend?")
+afstandKM = float(input("wat is de afstand in kilometers?"))
+if weekendrit == "ja":
+    weekendrit = True
+elif weekendrit == "nee":
+    weekendrit = False
+ritprijs(leeftijd, weekendrit, afstandKM)
